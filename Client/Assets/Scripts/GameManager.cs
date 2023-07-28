5323867abc7a3a9b59fbe63ec626b6eed40b63b8
Zhanyu Feng <zhanyu@argus.gg>
2023-07-28 16:03:20 -0700

    healthpack

--- Client/Assets/Scripts/GameManager.cs
+++ Client/Assets/Scripts/GameManager.cs
@@ -20,6 +20,7 @@ public class GameManager : MonoBehaviour
         die = 5,
         addHealth = 6,
         playerName = 9,
+        healthPackInfo =10,
         playerMove = 17,
     }
 
@@ -58,6 +59,12 @@ public class GameManager : MonoBehaviour
         public float Y;
         public int Value;
     }
+    struct HealthPack
+    {
+        public float X;
+        public float Y;
+        public int Value;
+    }
 
     struct PlayerName
     {
@@ -83,7 +90,9 @@ public class GameManager : MonoBehaviour
     public Transform coinsParent;
     public DamageTextSpawner dmgTextSpawner;
     public AttackAnimSpawner attackAnimSpawner;
-
+    public List<GameObject> healthPacks;
+    public GameObject healthPackPrefab;
+    public Transform healthPackParent;
 
     #endregion
    
@@ -140,7 +149,12 @@ public class GameManager : MonoBehaviour
             SpriteRenderer temp = Instantiate(coinPrefab, coinsParent);
             temp.gameObject.SetActive(false);
             coins.Add(temp);
+            var healthPack = Instantiate(healthPackPrefab, healthPackParent);
+            healthPack.SetActive(false);
+            healthPacks.Add(healthPack);
         }
+
+        
 
         // nakamaConnection.socket.ReceivedMatchmakerMatched += m => mainThread.Enqueue(() => OnReceivedMatchmakerMatched(m));
         nakamaConnection.socket.ReceivedMatchPresence += m => mainThread.Enqueue(() => OnReceivedMatchPresence(m));
@@ -351,7 +365,31 @@ public class GameManager : MonoBehaviour
                         coins[i].gameObject.SetActive(false);
                     }
                 }
-
+                break;
+            case (long) opcode.healthPackInfo:
+                List<HealthPack> HealthPackInfos;
+                try
+                {
+                    HealthPackInfos = content.FromJson<List<HealthPack>>();
+                }
+                catch (Exception e)
+                {
+                    print("content: " + content);
+                    Console.WriteLine(e);
+                    throw;
+                }
+                for (int i = 0; i < healthPacks.Count; i++)
+                {
+                    if (i < HealthPackInfos.Count)
+                    {
+                        healthPacks[i].gameObject.SetActive(true);
+                        healthPacks[i].transform.position = new Vector3(HealthPackInfos[i].X, HealthPackInfos[i].Y, 0);
+                    }
+                    else
+                    {
+                        healthPacks[i].gameObject.SetActive(false);
+                    }
+                }
                 break;
             case (long) opcode.attack:
                 List<Attack> attackInfos;
